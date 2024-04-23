1dfeb57902a3a51c8d7be38efd3aef285412c16e
CeicaIFCD0112 <rod.balseiro.ricardo@gmail.com>
2024-04-23 19:24:11 +0200

    first commit

---
+++ servicios.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inmertion On The Content</title>
+    <!-- Estilos de Bootstrap -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/jpg" href="/assets/img/fav-ico.jpg"/>
+    <style>
+        /* Estilo adicional para los formularios */
+        .form-container {
+            margin-top: 20px;
+            max-width: 400px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <h1>Inmertion On The Content</h1>
+    <table class="table">
+        <th>
+            <td><a href="index.php"><img src="media/img/i-home.jpg" alt=""></a></td>
+            <td><a href="servicios.php"><img src="media/img/i-servicios.jpg" alt=""></a></td>
+            <td><a href="proyectos.php"><img src="media/img/i-proyectos.jpg" alt=""></a></td>
+            <td><a href=""><img src="" alt=""></a></td>
+        </th>
+    </table>
+</div>
+<hr>
+<div class="container">
+    <table class="table">
+        <th>
+            <td><a href="https://ceicaifcd0112.pythonanywhere.com/"><img src="media/img/py1.jpg" width="50%" alt=""></a></td>
+            <td><a href="https://ceicaifcd0112.github.io/pokeweb/"><img src="media/img/jv1.jpg" width="50%" alt=""></a></td>
+            <td><a href="https://ceicaifcd0112.github.io/webby/"><img src="media/img/webby_1.jpg" width="50%" alt=""></a></td>
+            <td><img src="ruta_a_la_imagen_4.jpg" alt="" href=""></td>
+        </th>
+    </table>
+</div>
+<hr>
+<div class="container">
+    <table class="table">
+        <th>
+            <td><a href="http://www.facebook.com/"><img src="media/img/i-facebook.jpg" alt=""></a></td>
+            <td><a href="http://instagram.com/"><img src="media/img/i-instagram.jpg" alt=""></a></td>
+            <td><a href="https://web.whatsapp.com/"><img src="media/img/i-whatsapp.jpg" alt=""></a></td>
+            <td><img src="ruta_a_la_imagen_4.jpg" alt="" href=""></td>
+        </th>
+    </table>
+</div>
+</body>
+  <!-- Agrega los enlaces a Bootstrap JS (opcional, si necesitas funcionalidades adicionales de Bootstrap) -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</html>
